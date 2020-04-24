f1a69529277e4419d64d0c877a6f80b7a94750dc
apang <1305999617@qq.com>
2020-04-24 11:49:16 +0800

    'index模板，index管理页面，控制器，样式和layoutut_main'

--- hmsc/config/base_setting.py
+++ hmsc/config/base_setting.py
@@ -1,3 +1,16 @@
 SERVER_PORT = 9000
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Myp199706@127.0.0.1/hmsc_db?charset=utf8'
 SQLALCHEMY_TRACK_MODIFICATIONS = False  # 关闭了对模型的监控
+
+# Cookie
+AUTH_COOKIE_NAME = 'hmsc'
+
+# 设置拦截器规则
+IGNORE_URLS = [
+    '^/user/login'
+]
+
+IGNORE_CHECK_LOGIN_URLS = [
+    '^/static'
+    '^/favicon.ico'
+]
--- hmsc/web/controllers/user/User.py
+++ hmsc/web/controllers/user/User.py
@@ -1,76 +1,71 @@
-import json
-
-from flask import Blueprint, render_template, request, jsonify, make_response
+from flask import Blueprint,render_template,request,jsonify,make_response,redirect,g
 from common.models.user import User
 from common.libs.user.UserService import UserService
+from common.libs.UrlManager import UrlManager
+from common.libs.Helper import ops_render
 
+import json
 
-route_user = Blueprint('user_page', __name__)
+route_user = Blueprint('user_page',__name__)
 
-
-@route_user.route('/login', methods=['GET', 'POST'])
+@route_user.route("/login",methods=['GET','POST'])
 def login():
     if request.method == 'GET':
-
-        return render_template('user/login.html')
-
+        if g.current_user:
+            return redirect(UrlManager.buildUrl("/"))
+        return ops_render("user/login.html")
+        
+    # POST请求
     resp = {
-        'code': 200,
-        'msg': '登陆成功 @!@ ',
+        'code':200,
+        'msg':'登录成功',
         'data':{}
     }
     req = request.values
-    login_name = req['login_name']if 'login_name' in req else ''
-    login_pwd = req['login_pwd']if 'login_pwd' in req else ''
+    login_name = req['login_name'] if 'login_name' in req else ''
+    login_pwd = req['login_pwd'] if 'login_pwd' in req else ''
 
     if login_name is None or len(login_name) < 1:
         resp['code'] = -1
-        resp['msg'] = '请输入正确的用户名'
+        resp['msg'] = "请输入正确的用户名"
         return jsonify(resp)
     if login_pwd is None or len(login_pwd) < 1:
         resp['code'] = -1
-        resp['msg'] = '请输入正确的密码'
+        resp['msg'] = "请输入正确的密码"
         return jsonify(resp)
+    # 从数据库中取出user
     user_info = User.query.filter_by(login_name=login_name).first()
-    print(user_info)
     if not user_info:
         resp['code'] = -1
-        resp['msg'] = '用户不存在'
+        resp['msg'] = "用户不存在"
         return jsonify(resp)
     # 判断密码
-    if user_info.login_pwd != UserService.generatePwd(login_pwd, user_info.login_salt):
+    if user_info.login_pwd != UserService.generatePwd(login_pwd,user_info.login_salt):
         resp['code'] = -1
-        resp['msg'] = '密码输入错误'
+        resp['msg'] = "密码输入错误"
         return jsonify(resp)
-
+    
     # 判断用户状态
     if user_info.status != 1:
         resp['code'] = -1
-        resp['msg'] = '用户已经被禁用，请与管理员联系'
+        resp['msg'] = "用户已经被禁用，请联系管理员处理"
         return jsonify(resp)
-
-    # 加入cookie 状态
-    response = make_response(json.dumps({'code': 200, "msg": '登陆成功~~~'}))
-    # Cookie 中存入的信息是 user_info.login_name, user_info.uid
-    response.set_cookie('hmsc_1901C', '%s@%s'%(UserService.generateAuthCode(user_info), user_info.uid), 60*60*25*15)
-
+    
+    
+    response = make_response(json.dumps({'code':200,'msg':'登录成功~~~'}))
+    # Cookie中存入的信息是user_info.uid,user_info
+    response.set_cookie("hmsc_1901C","%s@%s"%(UserService.generateAuthCode(user_info),user_info.uid),60*60*24*15)
     return response
+    
 
-
-
-
-@route_user.route('/edit')
-def edit():
-
-    return '编辑'
-
-
-@route_user.route('/logout')
+@route_user.route("/logout")
 def logout():
+    return "登出"
 
-    return '登出'
+@route_user.route("/edit")
+def edit():
+    return "编辑"
 
-@route_user.route('/reset-pwd')
-def resetpwd():
-
-    return '重置密码'
+@route_user.route("/reset-pwd")
+def resetPwd():
+    return "重置密码"
